6ee6767308ecac5749aadca1b9d6565a54bfb5ec
Tejas Angadi <tejasangadi456@gmail.com>
2026-08-20 17:18:33 +0530

    feat: update dashboard service to fetch user details and return structured dashboard data

--- backend/src/dashboard/dashboard.module.ts
+++ backend/src/dashboard/dashboard.module.ts
@@ -4,9 +4,16 @@ import { JwtAuthModule } from 'src/common/jwt-auth.module';
 import DashboardService from './dashboard.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Room, RoomSchema } from 'src/rooms/room.schema';
+import { User, UserSchema } from 'src/users/user.schema';
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: Room.name, schema: RoomSchema }]), JwtAuthModule],
+  imports: [
+    MongooseModule.forFeature([
+      { name: Room.name, schema: RoomSchema },
+      { name: User.name, schema: UserSchema },
+    ]),
+    JwtAuthModule,
+  ],
   controllers: [DashboardController],
   providers: [DashboardService],
 })
--- backend/src/dashboard/dashboard.service.ts
+++ backend/src/dashboard/dashboard.service.ts
@@ -1,24 +1,45 @@
 import { Injectable, InternalServerErrorException, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { isNull } from 'lodash';
-import { Model } from 'mongoose';
+import { compact, find, get, isNil, isNull, pick } from 'lodash';
+import { Model, Types } from 'mongoose';
 import { PinoLogger } from 'nestjs-pino';
 import { Room, RoomDocument } from 'src/rooms/room.schema';
+import { User, UserDocument } from 'src/users/user.schema';
 
 @Injectable()
 class DashboardService {
   constructor(
     private readonly logger: PinoLogger,
     @InjectModel(Room.name) private roomModel: Model<RoomDocument>,
+    @InjectModel(User.name) private userModel: Model<UserDocument>,
   ) {}
 
   public async fetchDashboardDetails(userId: string, roomCode: string) {
     try {
-      const room: string | null = await this.roomModel.findOne({ roomCode });
+      const room: RoomDocument | null = await this.roomModel.findOne({ roomCode });
       if (isNull(room)) throw new NotFoundException('Room not found');
 
-      // From the room get the members. And fetch the user info of those members
-      return room;
+      const [member1, member2] = get(room, 'members');
+      const currentUser: Types.ObjectId = String(member1) === userId ? member1 : member2;
+      const otherUser: Types.ObjectId = currentUser === member1 ? member2 : member1;
+      const userIds: Types.ObjectId[] = compact([currentUser, otherUser]);
+      const users = await this.userModel.find({ _id: { $in: userIds } });
+
+      const currentUserDetails = find(users, (user) => String(user._id) === String(currentUser));
+      const otherUserDetails = isNil(otherUser)
+        ? null
+        : find(users, (user) => String(user._id) === String(otherUser));
+
+      if (isNil(currentUserDetails)) throw new NotFoundException('User not found');
+
+      const summaryFields = ['name', 'currentStreak', 'longestStreak'];
+      const dashboardDetails = {
+        roomCode,
+        currentUserDetails: pick(currentUserDetails, summaryFields),
+        otherUserDetails: isNil(otherUserDetails) ? null : pick(otherUserDetails, summaryFields),
+      };
+
+      return dashboardDetails;
     } catch (error) {
       if (error instanceof NotFoundException) throw error;
       throw new InternalServerErrorException('Failed to fetch dashboard data');
