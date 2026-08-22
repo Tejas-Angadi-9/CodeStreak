c64c68fc0158ff5b0cd25302b78e7410efd4da1e
Tejas Angadi <tejasangadi456@gmail.com>
2026-08-22 12:02:27 +0530

    feat: enhance dashboard service to support weekly activities and heatmap generation

--- backend/src/common/utils/date.util.ts
+++ backend/src/common/utils/date.util.ts
@@ -4,3 +4,21 @@ export function getTodaysDate(): string {
   const todaysDate = new Date().toLocaleDateString('en-CA', { timeZone: IST_TIME_ZONE });
   return todaysDate;
 }
+
+export function getWeekDates() {
+  const dates: string[] = [];
+  const currentDate = new Date(getTodaysDate());
+
+  while (true) {
+    const formattedDate = new Date(currentDate).toLocaleDateString('en-CA', {
+      timeZone: IST_TIME_ZONE,
+    });
+    dates.push(formattedDate);
+
+    if (currentDate.getDay() === 0) break;
+
+    currentDate.setDate(currentDate.getDate() - 1);
+  }
+
+  return dates;
+}
