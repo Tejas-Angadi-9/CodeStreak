80cb4fb819c41a11941791c36f0fbea3b81706e6
Tejas Angadi <tejasangadi456@gmail.com>
2026-09-12 22:31:15 +0530

    FE: Implement initial Google Sign-in integration (#21)

---
+++ frontend/src/pages/welcome/constants/welcomeRoutes.constant.tsx
@@ -0,0 +1,1 @@
+export const GOOGLE_AUTH = "/auth/google";
---
+++ frontend/src/pages/welcome/hooks/useGoogleAuth.ts
@@ -0,0 +1,46 @@
+import { useMutation } from "@tanstack/react-query";
+import { useNavigate } from "react-router-dom";
+import axiosClient from "../../../config/axiosClient";
+import { useGoogleLogin } from "@react-oauth/google";
+import { GOOGLE_AUTH } from "../constants/welcomeRoutes.constant";
+import type { IWelcomeLoginResponse } from "../interfaces/welcome.interface";
+
+const useGoogleAuth = () => {
+  const navigate = useNavigate();
+
+  const loginMutation = useMutation<IWelcomeLoginResponse, Error, string>({
+    mutationFn: async (authCode) => {
+      const loginResponse = await axiosClient.post(
+        GOOGLE_AUTH,
+        {
+          accessCode: authCode,
+        },
+        { withCredentials: true },
+      );
+      return loginResponse.data;
+    },
+    onSuccess: (data) => {
+      console.log("Login success response: ", data);
+      navigate("/");
+    },
+    onError: (error) => {
+      console.error("Login endpoint failed: ", error.message);
+    },
+  });
+
+  const login = useGoogleLogin({
+    flow: "auth-code",
+    onSuccess: (codeResponse) => {
+      loginMutation.mutate(codeResponse.code);
+    },
+    onError: (error) => console.log("Login failed: ", error),
+  });
+
+  return {
+    login,
+    isLoading: loginMutation.isPending,
+    error: loginMutation.error,
+  };
+};
+
+export default useGoogleAuth;
