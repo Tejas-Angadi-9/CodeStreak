7c71e90ed825f53356070ef38a14e320efaf5620
Tejas Angadi <tejasangadi456@gmail.com>
2026-09-12 22:09:32 +0530

    Feat: Add QueryClient configuration and integrate with QueryClientProvider in main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,11 +3,15 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import AppRoutes from "./routes";
 import { GoogleOAuthProvider } from "@react-oauth/google";
+import { QueryClientProvider } from "@tanstack/react-query";
+import queryClient from "./config/queryClient";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
-      <AppRoutes />
-    </GoogleOAuthProvider>
+    <QueryClientProvider client={queryClient}>
+      <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
+        <AppRoutes />
+      </GoogleOAuthProvider>
+    </QueryClientProvider>
   </StrictMode>,
 );
